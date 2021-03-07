41dd16a538b6646bb29dcacb6ab41f55a19379d8
陳鈞 <jim60105@gmail.com>
2021-03-07 13:53:19 +0800

    示範屬性繫結

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   sitename = 'The Will Will Web';
+  sitelogo = '/assets/images/logo.png';
 
   constructor() { }
 
